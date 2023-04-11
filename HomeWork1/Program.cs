eb23b56f5531f40c734ad0efbd9fbb0967c96718
Alexey <cappuccino13371@gmail.com>
2023-04-11 11:36:52 +0300

    add Seminar 2

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,82 +1,113 @@
-﻿// Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, 
-//         какое число большее, а какое меньшее.
+﻿// // Task 1. Напишите программу, которая на вход принимает два числа и выдаёт, 
+// //         какое число большее, а какое меньшее.
 
-Console.WriteLine("Введите первое число: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
 
-if(num1 > num2)
-{
-    Console.WriteLine($"Первое число {num1} больше чем второе {num2}");
-}
-else
-{
-    Console.WriteLine($"Второе число {num2} больше чем первое {num1}");
-}
-
-
-// Task 2. Напишите программу, которая принимает на вход три числа и 
-//         выдаёт максимальное из этих чисел.
-
-Console.WriteLine("Введите 3 числа:");
-int num_1 = Convert.ToInt32(Console.ReadLine());
-int num_2 = Convert.ToInt32(Console.ReadLine());
-int num_3 = Convert.ToInt32(Console.ReadLine());
-
-int max = num_1;
-
-if (num_2 > max)
-{
-    max = num_2;
-}
-
-if (num_3 > max)
-{
-    max = num_3;
-}
-
-Console.WriteLine("Наибольшее из введённых чисел: " + max);
+// if(num1 > num2)
+// {
+//     Console.WriteLine($"Первое число {num1} больше чем второе {num2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Второе число {num2} больше чем первое {num1}");
+// }
 
 
-//Task 3. Напишите программу, которая на вход принимает число и выдаёт, 
-//        является ли число чётным (делится ли оно на два без остатка).
+// // Task 2. Напишите программу, которая принимает на вход три числа и 
+// //         выдаёт максимальное из этих чисел.
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 3 числа:");
+// int num_1 = Convert.ToInt32(Console.ReadLine());
+// int num_2 = Convert.ToInt32(Console.ReadLine());
+// int num_3 = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 1)
-{
-    Console.WriteLine($"Число {num} является: Нечетным!");
-}
-else
-{
-    Console.WriteLine($"Число {num} является: Четным!");
-}
+// int max = num_1;
+
+// if (num_2 > max)
+// {
+//     max = num_2;
+// }
+
+// if (num_3 > max)
+// {
+//     max = num_3;
+// }
+
+// Console.WriteLine("Наибольшее из введённых чисел: " + max);
 
 
-//Task 4. Напишите программу, которая на вход принимает число (N), 
-//        а на выходе показывает все чётные числа от 1 до N.
+// //Task 3. Напишите программу, которая на вход принимает число и выдаёт, 
+// //        является ли число чётным (делится ли оно на два без остатка).
 
-int i = 1;
-bool not = true;
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите число: ");
-int num = Convert.ToInt32(Console.ReadLine());
+// if (num % 2 == 1)
+// {
+//     Console.WriteLine($"Число {num} является: Нечетным!");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {num} является: Четным!");
+// }
 
-Console.WriteLine($"Чётные числа от 1 до {num}");
-while(i <= num)
-{
-    if(i % 2 != 1)
-    {
-        Console.Write(i + ", ");
-        not = false;
-    }
-    i++;
-}
 
-if(not)
-{
-    Console.WriteLine("Нет чётных чисел!");
-}
+// //Task 4. Напишите программу, которая на вход принимает число (N), 
+// //        а на выходе показывает все чётные числа от 1 до N.
+
+// int i = 1;
+// bool not = true;
+
+// Console.Write("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine($"Чётные числа от 1 до {num}");
+// while(i <= num)
+// {
+//     if(i % 2 != 1)
+//     {
+//         Console.Write(i + ", ");
+//         not = false;
+//     }
+//     i++;
+// }
+
+// if(not)
+// {
+//     Console.WriteLine("Нет чётных чисел!");
+// }
+
+
+// Задача про двух друзей и собаку
+
+// int distance = 10000;
+// int first_friend_speed = 1;
+// int second_friend_speed = 2;
+// int dog_speed = 5;
+// int time;
+// int friend = 2;
+
+// int count = 0;
+
+// while(distance > 10)
+// {
+//     if(friend == 1)
+//     {
+//         time = distance / (first_friend_speed + dog_speed);
+//         friend = 2;
+//         distance = distance - (first_friend_speed + second_friend_speed) * time;       
+//     }
+//     count++;
+//     if(friend == 2)
+//     {
+//         time = distance / (second_friend_speed + dog_speed);
+//         friend = 1;
+//         distance = distance - (first_friend_speed + second_friend_speed) * time;
+//     }
+//     count++;
+// }
+// Console.WriteLine($"Собака пробежит {count} раз");
---
+++ Seminar2/Program.cs
@@ -0,0 +1,93 @@
+﻿// Task 1. Напишите программу, 
+//         которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int CutNumber(int num)
+{
+    int hundreds = num / 100;
+    int units = num % 10;
+
+    int result = hundreds * 10 + units;
+    return result;
+}
+
+int randNumber = new Random().Next(100, 1000);
+int shortNumber = CutNumber(randNumber);
+
+Console.WriteLine($"New version of of {randNumber} is {shortNumber}");
+
+
+// Task 2. Напишите программу, 
+//         которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int MaxDigits(int ranNumber)
+{
+    int units = ranNumber % 10;
+    int dozents = ranNumber / 10;
+    if (units > dozents) return units;
+    else return dozents;
+}
+int random = new Random().Next(10, 100);
+int randomNumber = MaxDigits(random);
+Console.WriteLine(random);
+Console.WriteLine(randomNumber);
+
+
+// Task 3. Напишите программу, которая будет принимать на вход два числа и выводить, 
+//        является ли второе число кратным первому.
+
+bool Even(int num1, int num2)
+{
+    if (num2 % num1 == 0)
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Input first number, please: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number, please: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Even(num1, num2);
+Console.WriteLine(result);
+
+// Task 4. Напишите программу, которая принимает на вход число n и проверяет, 
+//         кратно ли оно одновременно a и b.
+
+bool Even(int num1, int num2, int arg)
+{
+    if (num1 % arg == 0 && num2 % arg == 0)
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Input first number, please: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number, please: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input third number, please: ");
+int arg = Convert.ToInt32(Console.ReadLine());
+
+bool result = Even(num1, num2, arg);
+Console.WriteLine(result);
+
+
+// Task 5. Напишите программу, которая принимает на вход два числа и проверяет, 
+//         является ли одно число квадратом другого.
+
+bool Square(int num1, int num2)
+{
+    if ((num1 * num1 == num2) || (num2 * num2 == num1))
+        return true;
+    else
+        return false;
+}
+
+Console.WriteLine("Input first number, please: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input second number, please: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+bool result = Square(num1, num2);
+Console.WriteLine(result);
