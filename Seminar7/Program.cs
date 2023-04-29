90d65cec0ec75959ea59a6d8c27d5dec0082e4e6
Alexey <cappuccino13371@gmail.com>
2023-04-29 13:03:34 +0300

    update Seminar 7

--- Seminar7/Program.cs
+++ Seminar7/Program.cs
@@ -1,38 +1,38 @@
-﻿// int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
-// {
-//     int[,] array = new int[rows, colums];
+﻿int[,] CreateRandom2dArray(int rows, int colums, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, colums];
 
-//     for (int i = 0; i < rows; i++)
-//         for (int j = 0; j < colums; i++)
-//             array[i, j] = new Random().Next(maxValue, maxValue + 1);
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < colums; j++)
+            array[i, j] = new Random().Next(minValue, maxValue + 1);
 
-//     return array;
-// }
+    return array;
+}
 
-// void Show2dArray(int[,] array)
-// {
-//     for (int i = 0; i < array.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < array.GetLength(1); i++)
-//             Console.Write(array[i, j] + " ");
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i, j] + " ");
         
-//         Console.WriteLine();
-//     }
-//     Console.WriteLine();
-// }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
 
 
-// Console.Write("Input a quantity of rows: ");
-// int rows = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a quantity of colums: ");
-// int colums = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a quantity of min: ");
-// int minValue = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input a quantity of max: ");
-// int maxValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a quantity of colums: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a quantity of min: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input a quantity of max: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
 
-// int[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
-// Show2dArray(myArray);
+int[,] myArray = CreateRandom2dArray(rows, colums, minValue, maxValue);
+Show2dArray(myArray);
 
 
 // Task 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
